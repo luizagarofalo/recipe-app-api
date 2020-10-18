b0284e0b4d18adcefe575d7a9317f17c2f51046a
Luiza Garofalo <luiza.garofalo@gmail.com>
2020-10-17 23:02:14 -0300

    Add postgres database

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,7 +5,6 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        """Test creating a new user with an email is successful"""
         email = 'hello@luizagarofalo.com'
         password = 'test1234'
         user = get_user_model().objects.create_user(
@@ -17,7 +16,6 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        """Test if the e-mail for new user is normalized"""
         email = 'hello@LUIZAGAROFALO.com'
         user = get_user_model().objects.create_user(email, 'test1234')
 
